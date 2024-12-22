c246a65c408d5d9b8746e937bea97deb079f98c4
Eliav Menachi <eliav.menachi@gmail.com>
2024-12-22 21:23:29 +0200

    add swaGGER DOC

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,30 @@ import bodyParser from "body-parser";
 import posts_routes from "./routes/posts_routes";
 import comments_routes from "./routes/comments_routes";
 import auth_routes from "./routes/auth_routes";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/posts", posts_routes);
+app.use("/comments", comments_routes);
+app.use("/auth", auth_routes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Web Dev 2025 REST API",
+      version: "1.0.0",
+      description: "REST server including authentication using JWT",
+    },
+    servers: [{ url: "http://localhost:3000", },],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 const initApp = (): Promise<Express> => {
   return new Promise<Express>((resolve, reject) => {
@@ -21,11 +45,6 @@ const initApp = (): Promise<Express> => {
       mongoose
         .connect(process.env.DB_CONNECT)
         .then(() => {
-          app.use(bodyParser.json());
-          app.use(bodyParser.urlencoded({ extended: true }));
-          app.use("/posts", posts_routes);
-          app.use("/comments", comments_routes);
-          app.use("/auth", auth_routes);
           resolve(app);
         })
         .catch((err) => {
