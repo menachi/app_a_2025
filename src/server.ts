9d81d88320e41be146758ad068998c90c7e81391
Eliav Menachi <eliav.menachi@gmail.com>
2025-01-06 14:12:12 +0200

    forms

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,12 @@ import swaggerUI from "swagger-ui-express";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use("/posts", posts_routes);
 app.use("/comments", comments_routes);
 app.use("/auth", auth_routes);
